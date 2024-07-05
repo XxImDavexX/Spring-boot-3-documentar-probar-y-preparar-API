99a73590a45875ab8f3328e4d4cc28216241405e
XxImDavexX <david411dark@gmail.com>
2024-07-05 13:10:37 -0600

    Adicion del proyecto del curso aplique mejores practicas y proteja una API Rest

---
+++ src/main/java/med/voll/api/domain/direccion/Direccion.java
@@ -0,0 +1,79 @@
+package med.voll.api.domain.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Embeddable
+/*Usando los comandos de lombok*/
+@Getter //generammos los getters
+@NoArgsConstructor // construtor vacio
+@AllArgsConstructor //constructor con todos los argumentos
+public class Direccion {
+
+    private String calle;
+    private String distrito;
+    private String ciudad;
+    private String numero;
+    private String complemento;
+
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.ciudad();
+        this.numero = direccion.numero();
+        this.complemento = direccion.complemento();
+    }
+
+    public Direccion actualizarDatos(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.ciudad();
+        this.numero = direccion.numero();
+        this.complemento = direccion.complemento();
+        return this;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
